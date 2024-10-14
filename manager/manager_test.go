ee4704f749e7c8b604d336125ea6f9da350a6758
axe31803 <axe31803@bvm.network>
2024-10-14 10:41:16 +0700

    debug - load image 1

--- manager/manager_test.go
+++ manager/manager_test.go
@@ -3,7 +3,7 @@ package manager
 import "testing"
 
 func TestManager_LoadModel(t *testing.T) {
-	modelManager := NewModelManager("./models", "https://rpc.fluxchain.eternalai.org", "miner", "0x430583bdff80c5be1536ed82f9c8090eef68e2f6", false)
+	modelManager := NewModelManager("/home/models", "https://rpc.fluxchain.eternalai.org", "miner", "0x430583bdff80c5be1536ed82f9c8090eef68e2f6", false)
 	err := modelManager.LoadModelTest("0x9874732a8699fca824a9a7d948f6bcd30a141238")
 	if err != nil {
 		return
