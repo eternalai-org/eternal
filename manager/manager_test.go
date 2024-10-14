7f31ad79a376aed5ffa34dc771ef24663a0d68d0
axe31803 <axe31803@bvm.network>
2024-10-14 15:48:34 +0700

    add context 2 hours for testing

--- manager/manager_test.go
+++ manager/manager_test.go
@@ -1,8 +1,15 @@
 package manager
 
-import "testing"
+import (
+	"context"
+	"testing"
+	"time"
+)
 
 func TestManager_LoadModel(t *testing.T) {
+	_, cancel := context.WithTimeout(context.Background(), 2*time.Hour)
+	defer cancel() // Ensure resources are cleaned up
+
 	modelManager := NewModelManager("/home/models", "https://rpc.fluxchain.eternalai.org", "miner", "0x430583bdff80c5be1536ed82f9c8090eef68e2f6", false)
 	err := modelManager.LoadModelTest("0x9874732a8699fca824a9a7d948f6bcd30a141238")
 	if err != nil {
@@ -11,6 +18,9 @@ func TestManager_LoadModel(t *testing.T) {
 }
 
 func TestManager_LoadModelZkyncFalse(t *testing.T) {
+	_, cancel := context.WithTimeout(context.Background(), 2*time.Hour)
+	defer cancel() // Ensure resources are cleaned up
+
 	modelManager := NewModelManager("/home/models", "https://node.eternalai.org/", "miner", "0x05726BF187938c06d6C832dc493E3Df70fe735c8", false)
 	err := modelManager.LoadModelTest("0x8901edfde509d2ddc462cde2cea65f6e8ef27216")
 	if err != nil {
