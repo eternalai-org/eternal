b30468bf85a60fbac7c594b8e1e87a013d16db15
genesis0000 <genesis0000@bvm.network>
2024-10-21 10:23:47 +0700

    - update

--- task-watcher/watcher.go
+++ task-watcher/watcher.go
@@ -340,6 +340,18 @@ func (tskw *TaskWatcher) watchAndAssignTask() {
 			}
 		}
 
+		if len(tasks) == 0 {
+			modelAddr := tskw.GetAssignedModel()
+			log.Info("Watcher: Maybe node is slashed for model ", modelAddr)
+			if ok := tskw.isMinerOfModel(common.HexToAddress(modelAddr)); !ok {
+				log.Info("Watcher: node is reJoinMinting for model ", modelAddr)
+				err := tskw.reJoinMinting(modelAddr)
+				if err != nil {
+					log.Error("Watcher:reJoinMinting error", err)
+				}
+			}
+		}
+
 	}
 }
 
