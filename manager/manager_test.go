1ab166752a948f1664f2cae5260cd3dcf15f9e72
axe31803 <axe31803@bvm.network>
2024-10-24 11:39:22 +0700

    update code - 2

--- manager/manager_test.go
+++ manager/manager_test.go
@@ -19,8 +19,11 @@ func TestManager_LoadModel(t *testing.T) {
 	modelManager := NewModelManager("./home/models", "https://rpc.fluxchain.eternalai.org", "miner", "0x430583bdff80c5be1536ed82f9c8090eef68e2f6", false, true)
 	err := modelManager.LoadModelTest("0x9874732a8699fca824a9a7d948f6bcd30a141238")
 	if err != nil {
+		t.Log(err)
 		return
 	}
+
+	t.Log("ok")
 }
 
 /*
