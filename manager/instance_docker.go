b971a172d45ba4b845dca1f31eb1dd99713650b5
genesis0000 <genesis0000@bvm.network>
2024-11-28 15:02:36 +0700

    - update

--- manager/instance_docker.go
+++ manager/instance_docker.go
@@ -161,7 +161,7 @@ func (m *ModelInstance) SetupDocker() error {
 
 		//log.Info("[SetupDocker]  GetImageInfo: ", m.ModelInfo.ModelAddr, " ,img: ", img.ID)
 		if img.ID == "" {
-			m.ModelInfo.Metadata.ModelURL = "https://gateway.lighthouse.storage/ipfs/bafkreigdnqoe3hvlozmxnwc32qwskz3fvtuznpqzvp6o5crbnka2deczdu"
+			//Test: m.ModelInfo.Metadata.ModelURL = "https://gateway.lighthouse.storage/ipfs/bafkreigdnqoe3hvlozmxnwc32qwskz3fvtuznpqzvp6o5crbnka2deczdu"
 			temp := strings.Split(m.ModelInfo.Metadata.ModelURL, "/")
 			hash := temp[len(temp)-1]
 			out, err := zip_hf_model_to_light_house.DownloadHFModelFromLightHouse(hash, m.ModelPath, m.ZKSync)
