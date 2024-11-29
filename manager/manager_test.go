eebc99238ab7f1af5cb848021e3e31f70dd9a7b7
axe31803 <axe31803@bvm.network>
2024-11-29 16:08:47 +0700

    start docker

--- manager/manager_test.go
+++ manager/manager_test.go
@@ -7,11 +7,14 @@ import (
 	"time"
 )
 
-func TestManager_getFiles(t *testing.T) {
-	modelManager := NewModelManager("/home/models", "https://rpc.fluxchain.eternalai.org", "miner", "0x430583bdff80c5be1536ed82f9c8090eef68e2f6", false, true)
-	modelManager.RemoveTheGeneratedFile("0x9874732a8699fca824a9a7d948f6bcd30a141238")
-}
+/*
+	func TestManager_getFiles(t *testing.T) {
+		modelManager := NewModelManager("/home/models", "https://rpc.fluxchain.eternalai.org", "miner", "0x430583bdff80c5be1536ed82f9c8090eef68e2f6", false, true)
+		modelManager.RemoveTheGeneratedFile("0x9874732a8699fca824a9a7d948f6bcd30a141238")
+	}
 
+*
+*/
 func TestManager_LoadModel(t *testing.T) {
 	_, cancel := context.WithTimeout(context.Background(), 2*time.Hour)
 	defer cancel() // Ensure resources are cleaned up
@@ -23,6 +26,7 @@ func TestManager_LoadModel(t *testing.T) {
 	}
 }
 
+/*
 func TestManager_LoadModelZkyncFalse(t *testing.T) {
 	_, cancel := context.WithTimeout(context.Background(), 2*time.Hour)
 	defer cancel() // Ensure resources are cleaned up
@@ -33,3 +37,4 @@ func TestManager_LoadModelZkyncFalse(t *testing.T) {
 		return
 	}
 }
+*/
